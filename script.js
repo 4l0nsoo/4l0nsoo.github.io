dbfecf9db97ed191dcb4dc5b1f4cda8807c8af7b
4l0nsoo <169106024+4l0nsoo@users.noreply.github.com>
2024-05-09 11:36:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let var1 = "";
+let var2 = "";
+let var3 = "";
+let var4 = "";
+let var5 = "";
+let var6 = "";
+let var7 = "";
+let var8 = "";
+let var9 = "";
+let var10 = "";
 
+const selText = document.querySelector(".texto")
+selText.innerHTML("TEST")
