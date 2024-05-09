a63028fc21f546dc23727f563744b07b94db3329
4l0nsoo <169106024+4l0nsoo@users.noreply.github.com>
2024-05-09 12:11:12 -0500

    Update #4 script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let var1 = "";
-let var2 = "";
-let var3 = "";
-let var4 = "";
-let var5 = "";
-let var6 = "";
-let var7 = "";
-let var8 = "";
-let var9 = "";
-let var10 = "";
+let var1 = "___";
+let var2 = "___";
+let var3 = "___";
+let var4 = "___";
+let var5 = "___";
+let var6 = "___";
+let var7 = "___";
+let var8 = "___";
+let var9 = "___";
+let var10 = "___";
 
 const thetext = "En un lejano y misterioso bosque, vivía un grupo de" + var1 + "muy peculiares. Estos seres, conocidos como los" + var2 + ",eran famosos por su habilidad para" + var3 + "y su pasión por" + 4 + ". Cada mañana, al despertar, se reunían en la plaza del pueblo para" + var5 + "juntos y planear sus increíbles aventuras. Un día, mientras exploraban un sendero desconocido, se encontraron con un" + var6 + "gigante bloqueando su camino. Los valientes" + var2 + "no se amedrentaron y decidieron enfrentarse al desafío. Con sus" + var7 + "brillando de determinación, idearon un plan audaz para" + var8 + "al monstruo y continuar su camino hacia lo desconocido. Con astucia y trabajo en equipo, lograron" + var9 + "al gigante y despejar el sendero. Triunfantes, continuaron su viaje, sabiendo que juntos podían superar cualquier obstáculo que se interpusiera en su camino. Y así, con risas y" + var10 + "en el corazón, los" + var2 + "continuaron su aventura hacia lo desconocido, listos para enfrentar nuevos desafíos y descubrir los secretos que les esperaban."
 
 //const selText = document.querySelector(".texto");
 //selText.textContent("TEST");
 
-document.querySelector(".texto").textContent = (thetext)
+document.querySelector(".texto").textContent = "TEST";
